2f9a2af2b3bf3d99c6244e86771df3a07dcef374
Amir Y. Perehodnik <myrprhwdnyq@gmail.com>
2024-05-20 01:34:39 +0300

    Fix create-patch in the same manner

--- create-patch.sh
+++ create-patch.sh
@@ -15,9 +15,10 @@ mkdir -p patch/
 if [ ! -f "assets/js/app.js" ]; then
     echo "original app.js not found; downloading with curl"
     check_command curl
+    mkdir -p assets/js/ # if the user didnt use download-assets.py yet they likely dont have this directory
     curl "https://classic.minecraft.net/assets/js/app.js" -o "assets/js/app.js"
 fi
-prettier "assets/js/app.js" > "patch/original.js"
-prettier app.js > "patch/modified.js"
+prettier --ignore-path /dev/null "assets/js/app.js" > "patch/original.js"
+prettier --ignore-path /dev/null app.js > "patch/modified.js"
 
 diff -u "patch/original.js" "patch/modified.js" > "patch/mod.patch"
